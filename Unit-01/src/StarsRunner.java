dd893d556a0f56c8428347cac70d2c381978dffb
0StephenZhu <73717107+0StephenZhu@users.noreply.github.com>
2022-08-18 10:38:54 -0700

    Finish Unit-01 Instruction Lab

---
+++ Unit-01/src/StarsRunner.java
@@ -0,0 +1,30 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+	   
+	   StarsAndStripes tempObject = new StarsAndStripes();
+	   tempObject.printTwoBlankLines();
+	   tempObject.printTwentyStars();
+	   tempObject.printTwentyDashes();
+	   tempObject.printTwentyStars();
+	   tempObject.printTwentyDashes();
+	   tempObject.printASmallBox();
+	   tempObject.printABigBox();
+      
+      //call the methods needed to make the patterns on the word document
+      
+   }
+}
