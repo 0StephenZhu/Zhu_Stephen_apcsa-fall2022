17a78783463df443e0c09f239d2660732cfef14d
0StephenZhu <73717107+0StephenZhu@users.noreply.github.com>
2022-08-25 10:44:19 -0700

    Finished Unit-06

--- Unit-06/src/LetterRemover.java
+++ Unit-06/src/LetterRemover.java
@@ -11,10 +11,14 @@ public class LetterRemover
 
 	public LetterRemover()
 	{
-		//call set
+		setRemover("", ' ');
 	}
 
 	//add in second constructor
+	
+	public LetterRemover(String s, char rem) {
+		setRemover(s, rem);
+	}
 	
 	
 	public void setRemover(String s, char rem)
@@ -22,15 +26,30 @@ public class LetterRemover
 		sentence = s;
 		lookFor = rem;
 	}
+	
+	public int min(int one, int two) {
+		if (one < two) {
+			return one;
+		}
+		return two;
+	}
 
 	public String removeLetters()
 	{
 		String cleaned=sentence;
+		
+		while (cleaned.indexOf(lookFor) != -1) {
+			int index = cleaned.indexOf(lookFor);
+			//cleaned becomes substring 
+			
+			cleaned = cleaned.substring(0, index) + cleaned.substring(min(index + 1, cleaned.length()));
+		}
+		
 		return cleaned;
 	}
 
 	public String toString()
 	{
-		return sentence + " - letter to remove " + lookFor;
+		return sentence + " - letter to remove " + lookFor + "\n" + removeLetters();
 	}
 }
--- Unit-06/src/LetterRemoverRunner.java
+++ Unit-06/src/LetterRemoverRunner.java
@@ -9,6 +9,29 @@ public class LetterRemoverRunner
 	public static void main( String args[] )
 	{
 		//add test cases		
-											
+					
+		String[] testCasesString = {
+			"I am Sam I am   a",
+			"ssssssssxssssesssssesss",
+			"qwertyqwertyqwerty",
+			"abababababa",
+			"abaababababa",
+		};
+		
+		char[] testCasesChar = {
+			'a',
+			's',
+			'a',
+			'b',
+			'x'
+		};
+		
+		
+		LetterRemover temp = new LetterRemover();
+		
+		for (int i = 0; i < testCasesString.length; i++) {
+			temp.setRemover(testCasesString[i], testCasesChar[i]);
+			System.out.println(temp);
+		}
 	}
 }
